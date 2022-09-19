3acd170f307cfad9885efee73beb77cbcf113032
franck megret <msynthese@gmail.com>
2022-09-19 16:56:05 +0200

    mise en place devise + front

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_16_172138) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_19_075916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_16_172138) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "prenom"
+    t.string "nom"
+    t.date "naissance"
+    t.string "adresse"
+    t.string "cp"
+    t.string "ville"
+    t.string "pays"
+    t.string "telephone_fixe"
+    t.string "telephone_portable"
+    t.boolean "admin", default: false
+    t.boolean "responsable", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
