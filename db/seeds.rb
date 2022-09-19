aef3ff1702166681b28759b609d339cc986bdfff
franck megret <msynthese@gmail.com>
2022-09-19 17:28:08 +0200

    .....

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# USERS SEED
+puts "Destroying Users..."
+User.destroy_all
+puts "Creating users..."
+user_1 = { prenom: "Franck", nom: "Megret", email: "msynthese@gmail.com", password: "123456", naissance: "1973-05-21", adresse: "114 rue la vie du char", cp: "01130", ville: "Echallon", pays: "France", telephone_portable: "07-86-29-45-52", admin: true }
+user_2 = { prenom: "Melanie", nom: "Megret", email: "melanie01550@hotmail.fr", password: "123456", naissance:" 1979-08-13", adresse: "114 rue la vie du char", cp: "01130", ville: "Echallon", pays: "France", telephone_portable: "06-09-05-05-97" }
+[user_1, user_2].each do |attributes|
+  user = User.create!(attributes)
+  puts "Created #{user.email}"
+end
+puts "All Users created!"
