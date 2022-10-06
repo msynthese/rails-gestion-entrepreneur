3e7afab79375534e83f14523eb98a1eec2eebd4b
franck megret <msynthese@gmail.com>
2022-10-06 19:56:03 +0200

    update

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,4 +3,5 @@ class Document < ApplicationRecord
   belongs_to :client
   enum types: %i[Devis Facture]
   enum statut: %i[Creation Attente Accepté Refusé Terminé]
+  validates :designation, presence: true
 end
